9a07128e8b17c28e3ddde780eaa1321d42ad7698
Sajal K <sajalsaju687@gmail.com>
2024-08-09 16:38:59 +0530

    fix: media root change in settings

--- serverside/settings.py
+++ serverside/settings.py
@@ -119,7 +119,7 @@ STATIC_URL = 'static/'
 # ]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR / "media")
+MEDIA_ROOT = '/app/media/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
